c39c9eff27741b8737988d8283f3fde3c704cb6b
Kelvin <48173358+kelvintan323@users.noreply.github.com>
2026-01-20 20:34:06 +0800

    Update: Check admin seeder first

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,10 +15,12 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::factory()->create([
-            'name' => 'Test Admin',
-            'email' => 'admin@test.com',
-            'password' => bcrypt(12345678),
-        ]);
+        if (!Admin::where('email', 'admin@test.com')->exists()) {
+            Admin::factory()->create([
+                'name' => 'Test Admin',
+                'email' => 'admin@test.com',
+                'password' => bcrypt(12345678),
+            ]);
+        }
     }
 }
